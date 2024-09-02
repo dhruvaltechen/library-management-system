3c0b3a2ae6db239b13478237f2dc21f46a110d5d
dhruvaltechen <dhruval@techeniac.com>
2024-09-02 11:05:03 +0530

    fix: modified return  book logic

--- src/return_book.py
+++ src/return_book.py
@@ -3,10 +3,11 @@ from _globals._books import AVAILABLE_BOOKS, ALL_BOOKS_ISBN
 def return_book(isbn):
     for book in AVAILABLE_BOOKS:
         if book['isbn'] == isbn:
-            print('Book is already retured')
+            print(book['title'], 'by', book['author'], 'is alrady returned')
             return book
     for book in ALL_BOOKS_ISBN:
         if book['isbn'] == isbn:
             AVAILABLE_BOOKS.append(book)
+            print(book['title'], 'by', book['author'], 'returned successfully')
             return book
     print('Book not found')
