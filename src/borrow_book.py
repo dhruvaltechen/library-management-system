852d1cd7b0dac3fdc39c181e350598aa56672a9c
dhruvaltechen <dhruval@techeniac.com>
2024-09-02 11:04:42 +0530

    fix: modified globals and borrow book logic

--- src/borrow_book.py
+++ src/borrow_book.py
@@ -1,8 +1,12 @@
-from _globals._books import AVAILABLE_BOOKS, ALL_BOOKS_ISBN
+from _globals._books import AVAILABLE_BOOKS
 
 def borrow_book(isbn):
+    if len(AVAILABLE_BOOKS) == 0:
+        print('No Books Available')
+        return
     for book in AVAILABLE_BOOKS:
         if book['isbn'] == isbn:
             AVAILABLE_BOOKS.remove(book)
+            print(book['title'], 'by', book['author'], 'borrowed successfully')
             return book
     return None
