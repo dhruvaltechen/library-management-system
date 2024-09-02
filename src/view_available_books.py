60c628bd23a61e87eaeccfa2b1dc52203cf459fd
dhruvaltechen <dhruval@techeniac.com>
2024-09-02 11:05:35 +0530

    fix: modified available books logic

--- src/view_available_books.py
+++ src/view_available_books.py
@@ -1,5 +1,9 @@
 from _globals._books import AVAILABLE_BOOKS
 
 def view_available_books():
+    if len(AVAILABLE_BOOKS) == 0:
+        print('No Books Available')
+        return
+    print('AVAILABLE BOOKS')
     for book in AVAILABLE_BOOKS:
         print(f'ISBN: {book["isbn"]}, Title: {book["title"]}, Author: {book["author"]}, Publication Year: {book["publication_year"]}')
